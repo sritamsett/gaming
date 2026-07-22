b3632194e39ae1a193284c59268ab9f5b8b34850
sritamsett <168722871+sritamsett@users.noreply.github.com>
2026-07-22 21:19:19 +0530

    Update b.py

--- Small/b.py
+++ Small/b.py
@@ -1,27 +1,15 @@
 import turtle
-
 t = turtle.Turtle()
 t.speed(3)
-
 t.pensize(6)
-
-t.penup()
-t.goto(-50, 0)
-t.setheading(90)
 t.pendown()
-
-t.forward(140)
-t.right(180)
-t.penup()
-t.forward(140)
-t.right(180)
-t.forward(5)
 t.right(90)
-t.pendown()
+t.forward(140)
 
+
+t.backward(70)
+t.left(90)
 t.forward(40)
-t.circle(30, 180)
-
+t.circle(-30, 180)
 t.forward(40)
-
 turtle.done()
