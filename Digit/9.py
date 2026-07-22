dfccf6614edd0e42650d2aa14e193f178fdc7883
sritamsett <168722871+sritamsett@users.noreply.github.com>
2026-07-22 20:44:13 +0530

    Update 9.py

--- Digit/9.py
+++ Digit/9.py
@@ -1,18 +1,18 @@
 import turtle
-
 t = turtle.Turtle()
 t.speed(3)
-
 t.pensize(6)
-
-t.penup()
-t.goto(-50, -50)
-t.setheading(0)
 t.pendown()
 
-t.right(30)
-t.circle(60, 200)
-t.circle(30, 300)
+t.left(90)
+t.forward(10)
+t.circle(-20,180)
+t.forward(10)
+t.circle(-20,180)
+t.forward(10)
+t.circle(-20,180)
+t.forward(50)
+t.circle(-20,160)
 
-
+t.penup()
 turtle.done()
